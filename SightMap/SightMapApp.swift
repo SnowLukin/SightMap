12500e299db0fcc2e3e80bef996de065725756ef
Snow Lukin <everyoursden@gmail.com>
2022-01-24 23:23:20 +0300

    Initial Commit

---
+++ SightMap/SightMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SightMapApp.swift
+//  SightMap
+//
+//  Created by Snow Lukin on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SightMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
